738ec5146fa31cc3185e3d55812900cb338052d1
Felipe Zani <59931479+FelipitoBr@users.noreply.github.com>
2023-04-15 14:59:28 +0200

    Update rpg.c
    pas de variables globales et la position du player attribué par struct

--- rpg.c
+++ rpg.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "tc.h"
+typedef struct
+{
+	int x;
+	int y;
+} Player;
 
-#define MAP_SIZE 100
-
-#define MOVE_UP 'z'
-#define MOVE_DOWN 's'
-#define MOVE_LEFT 'q'
-#define MOVE_RIGHT 'd'
-
-int map[MAP_SIZE][MAP_SIZE];
-int player_x, player_y;
-
-void init_map(){
+void init_map(int map[][MAP_SIZE]){
 	for (int i = 0; i < MAP_SIZE; i++){
 		for (int k = 0; k < MAP_SIZE; k++) {
 		map[i][k] = 0;
@@ -20,11 +16,11 @@ void init_map(){
 	}
 }
 
-void print_map(){
+void print_map(Player player, int map[][MAP_SIZE]){
 	system("clear");
 	for (int i = 0; i < MAP_SIZE; i++){
 		for (int k = 0; k < MAP_SIZE; k++) {
-			if (i == player_y && k == player_x) {
+			if (i == player.y && k == player.x) {
 				printf("P ");
 			} else {
 				printf(". ");
@@ -34,9 +30,9 @@ void print_map(){
 	}
 }
 
-void move_player(char input) {
-	int new_x = player_x;
-	int new_y = player_y;
+void move_player(char input, Player player, int map[][MAP_SIZE] ) {
+	int new_x = player.x;
+	int new_y = player.y;
 	switch (input) {
 		case MOVE_UP :
 			new_y--;
@@ -52,27 +48,23 @@ void move_player(char input) {
 			break;
 		}
 	if (new_x >= 0 && new_x < MAP_SIZE && new_y >= 0 && new_y < MAP_SIZE && map[new_x][new_y] == 0) {
-	player_x = new_x;
-	player_y = new_y;
+	player.x = new_x;
+	player.y = new_y;
 	}
 }
 
 int main() {
-	init_map();
-	player_x = 50;
-	player_y = 50;
+	int map[MAP_SIZE][MAP_SIZE];
+	Player player;
+	init_map(map);
+	player.x = 50;
+	player.y = 50;
 	char input;
 	while (1) {
-		print_map();
+		print_map(player,map);
 		printf("\nup:%c left:%c down:%c right:%c\n\n", MOVE_UP, MOVE_LEFT, MOVE_DOWN, MOVE_RIGHT);
 	scanf("%c", &input);
-	move_player(input);
+	move_player(input,player, map);
 	}
 	return 0;
 }
-
-
-
-
-
-
